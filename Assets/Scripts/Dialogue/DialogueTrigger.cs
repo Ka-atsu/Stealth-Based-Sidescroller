3a3106e527f4716b4b827afd11b92f8d1554575c
RKTrussel <129744087+RKTrussel@users.noreply.github.com>
2026-03-12 12:09:08 +0800

    Refactor dialogue system and add animations
    Restructure the dialogue system to support per-line characters and icons and integrate UI/animator changes. Deleted the old Dialogue class and added serializable DialogueCharacter, DialogueLine and Dialogue (list) types; updated DialogueManager to a singleton, renamed DisplayNextSentence to DisplayNextDialogueLine, queue DialogueLine objects, show character icon/name, toggle animator IsOpen, and change the continue button text to "Exit" on the last line. Updated DialogueTrigger to use DialogueManager.Instance. Scene (DialogueTesting.unity) was updated with new component references and prefab entries. Added Dialogue UI animations and an AnimatorController under Assets/Animations/DialogueAnimation, and added a _Recovery scene.

--- Assets/Scripts/Dialogue/DialogueTrigger.cs
+++ Assets/Scripts/Dialogue/DialogueTrigger.cs
@@ -1,4 +1,28 @@
+using NUnit.Framework;
+using System.Collections.Generic;
 using UnityEngine;
+
+
+[System.Serializable]
+public class DialogueCharacter
+{
+    public string name;
+    public Sprite icon;
+}
+
+[System.Serializable]
+public class DialogueLine
+{
+    public DialogueCharacter character;
+    [TextArea(3, 10)]
+    public string line;
+}
+
+[System.Serializable]
+public class Dialogue
+{
+    public List<DialogueLine> dialogueLines = new List<DialogueLine>();
+}
 
 public class DialogueTrigger : MonoBehaviour
 {
@@ -6,6 +30,6 @@ public class DialogueTrigger : MonoBehaviour
 
     public void TriggerDialogue()
     {
-        FindFirstObjectByType<DialogueManager>().StartDialogue(dialogue);
+        DialogueManager.Instance.StartDialogue(dialogue);
     }
 }
